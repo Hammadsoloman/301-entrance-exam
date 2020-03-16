4c9cb30a045acf0ef4b8f7adeb794aea7e5025a7
Hammadsoloman <hammadali95@yahoo.com>
2020-03-16 12:44:07 +0200

    new solution

---
+++ js/q1.js
@@ -0,0 +1,14 @@
+var a =[1,2,3,4];
+var b ;
+var coun=0;
+
+function graterThan(a,b){
+
+    for(var i=0;i<a.length;1++){
+        if(b > a[i]){
+          count++;
+        }
+    }
+    return(count);
+}
+
